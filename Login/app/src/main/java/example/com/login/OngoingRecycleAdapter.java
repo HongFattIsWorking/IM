9909969e11d38e97df22ae21bd126e5e43593a70
DESKTOP-7JNM90H\kelvi <kelvinang94@gmail.com>
2018-11-04 15:25:45 +0800

    ongoing recycle

---
+++ Login/app/src/main/java/example/com/login/OngoingRecycleAdapter.java
@@ -0,0 +1,37 @@
+package example.com.login;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class OngoingRecycleAdapter extends RecyclerView.Adapter<OngoingRecycleAdapter.ViewHolder> {
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this,itemView);
+        }
+    }
+}
